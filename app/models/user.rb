a910ccb5160f68c851b205cf92e18f1e0386d200
Gabriel Valle <gvalle91@gmail.com>
2018-07-13 01:47:10 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
   validates_presence_of :username, :email, :password
+<<<<<<< HEAD
 
   def slug
     username.downcase.gsub(" ","-")
@@ -10,4 +11,6 @@ class User < ActiveRecord::Base
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
   end
+=======
+>>>>>>> 447731662e3c55971994b4a2635472b148432720
 end
